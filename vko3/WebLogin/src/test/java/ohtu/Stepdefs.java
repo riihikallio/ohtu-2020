32525edf07bfef61caae41ebb24d4d997b1d1689
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-11-15 23:03:15 +0200

    V3T9

--- vko3/WebLogin/src/test/java/ohtu/Stepdefs.java
+++ vko3/WebLogin/src/test/java/ohtu/Stepdefs.java
@@ -37,6 +37,11 @@ public class Stepdefs {
     public void correctUsernameAndIncorrectPasswordAreGiven(String username, String password) {
         logInWith(username, password);
     }    
+ 
+    @When("incorrect username {string} and valid password {string} are given")
+    public void incorrectUsernameAndValidPasswordAreGiven(String username, String password) {
+        logInWith(username, password);
+    }    
     
     @Then("user is not logged in and error message is given")
     public void userIsNotLoggedInAndErrorMessageIsGiven() {
