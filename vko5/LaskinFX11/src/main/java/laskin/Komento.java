5eef5831526abf988f296ef8b794e5b110ae357c
Petri Riihikallio <petri.riihikallio@metis.fi>
2020-11-28 00:47:05 +0200

    V5T5

---
+++ vko5/LaskinFX11/src/main/java/laskin/Komento.java
@@ -0,0 +1,53 @@
+package laskin;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public abstract class Komento {
+
+    private TextField tuloskentta;
+    private TextField syotekentta;
+    private Button nollaa;
+    private Button undo;
+    protected Sovelluslogiikka sovellus;
+    protected int arvo;
+    private int edellinen = 0;
+
+    public Komento(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        this.tuloskentta = tuloskentta;
+        this.syotekentta = syotekentta;
+        this.nollaa = nollaa;
+        this.undo = undo;
+        this.sovellus = sovellus;
+    }
+
+    public void suorita() {
+        try {
+            edellinen = Integer.parseInt(tuloskentta.getText());
+            arvo = Integer.parseInt(syotekentta.getText());
+        } catch (Exception e) {
+            arvo = 0;
+        }
+    }
+
+    public void peru() {
+        sovellus.aseta(edellinen);
+        try {
+            edellinen = Integer.parseInt(tuloskentta.getText());
+        } catch (Exception e) { }
+        esita();
+    }
+
+    public void esita() {
+        int laskunTulos = sovellus.tulos();
+        syotekentta.setText("");
+        tuloskentta.setText("" + laskunTulos);
+
+        if (laskunTulos == 0) {
+            nollaa.disableProperty().set(true);
+        } else {
+            nollaa.disableProperty().set(false);
+        }
+        undo.disableProperty().set(false);
+    }
+}
---
+++ vko5/LaskinFX11/src/main/java/laskin/Summa.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laskin;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Summa extends Komento {
+
+    public Summa(TextField tuloskentta, TextField syotekentta, Button nollaa, Button undo, Sovelluslogiikka sovellus) {
+        super(tuloskentta, syotekentta, nollaa, undo, sovellus);
+    }
+
+    @Override
+    public void suorita() {
+        super.suorita();
+        sovellus.plus(arvo);
+        super.esita();
+    }
+
+}
